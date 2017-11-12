826ab3e9fb99dfb2ea4fb0770cd569864b56fc5d
on-three <on.three.email@gmail.com>
2017-11-11 23:15:40 -0800

    Added colored background to imag render.

--- Makefile
+++ Makefile
@@ -7,6 +7,9 @@ SCRIPT ?= $(SCRIPTS_DIR)/test.py
 
 BLENDER := blender
 
+# tool to view images
+DISPLAY := display
+
 IMG ?= $(OUT_DIR)/test.jpg
 
 run: $(IMG)
@@ -14,6 +17,9 @@ run: $(IMG)
 $(IMG): $(SCRIPT)
 	mkdir -p $(@D)
 	$(BLENDER) --background --python $< $(IMG)
+
+show: $(IMG)
+	$(DISPLAY) $<
 
 clean:
 	rm -fr $(OUT_DIR)
--- scripts/test.py
+++ scripts/test.py
@@ -1,36 +1,69 @@
 # Run as: blender -b <filename> -P <this_script> -- <args>
-import bpy, sys, os
+import bpy
+import sys, os
 
-# you can catch command line arguments this way
-imagePath = sys.argv[-1]
+def setRenderSettings():
+  render = bpy.context.scene.render
+  render.resolution_x = 720
+  render.resolution_y = 576
+  render.resolution_percentage = 100
+  render.fps = 24    
+  render.use_raytrace = False
+  #render.use_color_management = True
+  render.use_sss = False
+  return
 
-#bpy.ops.object.text_add()
-#obj = bpy.context.object
-#tcu = obj.data
+if __name__ == '__main__':
+  # you can catch command line arguments this way
+  imagePath = sys.argv[-1]
 
-#print(ob, tcu)
+  # clear everything
+  #bpy.ops.wm.read_factory_settings(use_empty=True)
 
-# TextCurve attributes
-#tcu.body = "Hello, world"
-#tcu.font = bpy.data.fonts[0]
-#print("Font", tcu.font)
-#tcu.offset_x = 0.1
-#tcu.offset_y = -0.25
-#tcu.shear = 0.5
-#tcu.spacing = 2
-#tcu.ul_height = 0.7
-#tcu.ul_position = -1
-#tcu.word_spacing = 4
+  setRenderSettings()
+  
+  #bpy.ops.object.text_add()
+  #obj = bpy.context.object
+  #tcu = obj.data
 
-# Inherited Curve attributes
-#tcu.extrude = 0.2
-#tcu.back = True
+  #print(ob, tcu)
 
-# Render to separate file, identified by texture file
-#imageBaseName = bpy.path.basename(imagePath)
-bpy.context.scene.render.filepath = imagePath
+  # TextCurve attributes
+  #tcu.body = "Hello, world"
+  #tcu.font = bpy.data.fonts[0]
+  #print("Font", tcu.font)
+  #tcu.offset_x = 0.1
+  #tcu.offset_y = -0.25
+  #tcu.shear = 0.5
+  #tcu.spacing = 2
+  #tcu.ul_height = 0.7
+  #tcu.ul_position = -1
+  #tcu.word_spacing = 4
 
-# Render still image, automatically write to output path
-bpy.ops.render.render(write_still=True)
+  # Inherited Curve attributes
+  #tcu.extrude = 0.2
+  #tcu.back = True
 
-bpy.ops.wm.quit_blender()
+  # World settings
+  world = bpy.context.scene.world
+  world.use_sky_blend = True
+  world.ambient_color = (0.0, 0, 0)
+  world.horizon_color = (0, 0, 0.2)
+  world.zenith_color = (0.04, 0, 0.04)
+
+  # Environment lighting
+  wset = world.light_settings
+  wset.use_environment_light = True
+  wset.use_ambient_occlusion = True
+  wset.ao_blend_type = 'MULTIPLY'
+  wset.ao_factor = 0.8
+  wset.gather_method = 'APPROXIMATE'
+
+  # Render to separate file, identified by texture file
+  #imageBaseName = bpy.path.basename(imagePath)
+  bpy.context.scene.render.filepath = imagePath
+
+  # Render still image, automatically write to output path
+  bpy.ops.render.render(write_still=True)
+
+  bpy.ops.wm.quit_blender()
