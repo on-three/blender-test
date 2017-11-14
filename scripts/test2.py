dc9645dfef7d1915bc162c337080d3f494a2a204
on-three <on.three.email@gmail.com>
2017-11-13 23:48:16 -0800

    Basic audio in rendered video.

--- scripts/test2.py
+++ scripts/test2.py
@@ -14,12 +14,29 @@ def setRenderSettings():
   render.use_sss = False
   return
 
+def setup_scene():
+  scene = bpy.context.scene
+  if not scene.sequence_editor:
+    scene.sequence_editor_create()
+
+  soundstrip = scene.sequence_editor.sequences.new_sound("1", "audio/1.cia.mp3", 3, 1)
+  end = soundstrip.frame_final_end #frame_duration
+  soundstrip = scene.sequence_editor.sequences.new_sound("2", "audio/2.bane.mp3", 3, end)
+  end = soundstrip.frame_final_end #end + soundstrip.frame_duration
+  soundstrip = scene.sequence_editor.sequences.new_sound("3", "audio/3.cia.mp3", 3, end)
+  end = soundstrip.frame_final_end #end + soundstrip.frame_duration
+  soundstrip = scene.sequence_editor.sequences.new_sound("4", "audio/4.bane.mp3", 3, end)
+
 
 if __name__ == '__main__':
   # you can catch command line arguments this way
   filePath = sys.argv[-1]
   
   setRenderSettings()
+
+  setup_scene()
+
+  # render
   for scene in bpy.data.scenes:
     scene.render.image_settings.file_format = 'H264'
     scene.render.ffmpeg.format = 'QUICKTIME'
@@ -27,8 +44,8 @@ if __name__ == '__main__':
     scene.render.ffmpeg.audio_codec = 'AAC'
   scene.render.ffmpeg.audio_bitrate = 128
   scene.render.resolution_percentage = 100
-  bpy.context.scene.frame_start = 0
-  bpy.context.scene.frame_end = 24
+  #bpy.context.scene.frame_start = 0
+  #bpy.context.scene.frame_end = 24
   bpy.context.scene.render.filepath = filePath
   bpy.ops.render.render(animation = True, write_still = False)
 
