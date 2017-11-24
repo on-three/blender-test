d57c0c7161e2b0451b6fb6026af3991123454e00
on-three <on.three.email@gmail.com>
2017-11-24 10:11:01 -0800

    Added improved phonemes outputs

--- scripts/phonemes.sh
+++ scripts/phonemes.sh
@@ -17,7 +17,7 @@ INFILE=$1
 RAW_AUDIO=${INFILE}.raw
 SOX=sox
 SAMPLE_RATE=16000
-$SOX $INFILE -r $SAMPLE_RATE $RAW_AUDIO
+$SOX $INFILE -r $SAMPLE_RATE --bits 16 --encoding signed-integer --endian little $RAW_AUDIO
 
 # run the audio file through pocketsphinx_continuous and get raw phonemes
 
@@ -25,10 +25,12 @@ SPHINX=pocketsphinx_continuous
 MODEL_PATH=./sphinx/model/en-us
 HMM_PATH=${MODEL_PATH}/en-us/
 ALLPHONE_FILE=${MODEL_PATH}/en-us-phone.lm.bin
-ADDITIONAL_ARGS="-time yes -beam 1e-10 -pbeam 1e-10 -lw 0.5"
+#ADDITIONAL_ARGS="-time yes -beam 1e-10 -pbeam 1e-10 -lw 0.5"
+ADDITIONAL_ARGS="-time yes -dither no -beam 1e-10 -pbeam 1e-10 -lw 1.5"
 
 OUTFILE=${INFILE}.phonemes.out.txt
 
 $SPHINX -infile $RAW_AUDIO -hmm $HMM_PATH -allphone $ALLPHONE_FILE $ADDITIONAL_ARGS > $OUTFILE
+#$SPHINX -infile $RAW_AUDIO -hmm $HMM_PATH -allphone $ALLPHONE_FILE $ADDITIONAL_ARGS
 
 
