b1302f5c4dc051c7f30a664df14e9deceabbecbc
on-three <on.three.email@gmail.com>
2017-12-06 02:24:16 -0800

    Added basic get post script.

---
+++ scripts/get_post.js
@@ -0,0 +1,42 @@
+var page = require('webpage').create();
+var system = require('system');
+
+var args = system.args
+//var url = 'http://google.com'
+var url = 'https://boards.fireden.net/a/thread/145649654'
+//var s = '#hplogo'
+var s = "#\\31 45650295"
+var file = 'google.png'
+
+if(args.length > 1)
+{
+  url = args[1];
+}
+//if(args.length > 2)
+//{
+//  s = args[2];
+//}
+//if(args.length > 3)
+//{
+//  file = args[3]
+//}
+
+//var s = "#hplogo"      
+page.open(url, function() {
+    // being the actual size of the headless browser
+    page.viewportSize = { width: 768, height: 1024 };
+    var clipRect = page.evaluate(function(s){
+      //return document.querySelector('#hplogo').getBoundingClientRect();
+      return document.querySelector(s).getBoundingClientRect();
+      //var selector = '#hplogo'
+    },s);
+    page.clipRect = {
+      top:    clipRect.top,
+      left:   clipRect.left,
+      width:  clipRect.width,
+      height: clipRect.height
+    };
+      
+    page.render(file);
+      phantom.exit();
+});
