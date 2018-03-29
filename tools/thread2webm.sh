f19661719248281dc89016210417f34647b8cf4e
on-three <on.three.email@gmail.com>
2018-03-29 01:04:46 -0700

    Added no text post support. Corrected output filename.

--- tools/thread2webm.sh
+++ tools/thread2webm.sh
@@ -33,7 +33,8 @@ VIDEO_LIST=${POST_LIST}.videos.txt
 rm -f ${VIDEO_LIST}
 touch ${VIDEO_LIST}
 
-THREAD_WEBM=output.webm
+BASENAME=$(basename "$POST_LIST" .txt)
+THREAD_WEBM=${BASENAME}.webm
 
 IFS=$'\n'
 set -f
@@ -46,9 +47,10 @@ for POST_NUM in $(cat < ${POST_LIST}); do
   POST_AUDIO=${WORKING_DIR}/${POST_NUM}.mp3
   POST_VIDEO=${WORKING_DIR}/${POST_NUM}.mp4
   IMG_SIZE=1024x768
+  NO_TEXT_DURATION_S=4
 
   # generate an image and textfile off the post
-  if [ ! -f $POST_IMG ] || [ ! -f $POST_TXT ]; then
+  if [ ! -f $POST_IMG ]; then
     phantomjs tools/get_post.js "$THREAD_URL" "$POST_NUM" "$WORKING_DIR"
     convert $POST_IMG -gravity center -background black -resize $IMG_SIZE -extent $IMG_SIZE $POST_IMG
   fi
@@ -58,15 +60,14 @@ for POST_NUM in $(cat < ${POST_LIST}); do
     echo "Post file does not exist. FAILING"
     exit -1
   fi
-  if [ ! -f $POST_TXT ]; then
-    echo "Post text does not exist. FAILING"
-    exit -1
-  fi
-
 
   # generate TTS audio
   if [ ! -f $POST_AUDIO ]; then
-    gtts-cli -f ${POST_TXT} -o ${POST_AUDIO}
+    if [ ! -f $POST_TXT ]; then
+      ffmpeg -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=24000 -t ${NO_TEXT_DURATION_S} ${POST_AUDIO}
+    else
+      gtts-cli -f ${POST_TXT} -o ${POST_AUDIO}
+    fi
   fi
 
   AUDIO_LENGTH=`ffmpeg -i ${POST_AUDIO} 2>&1 |grep -oP "[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{2}"`
@@ -80,8 +81,10 @@ for POST_NUM in $(cat < ${POST_LIST}); do
   echo $POST_AUDIO
   echo $POST_IMG
   echo $POST_VIDEO
-  ffmpeg -y -loop 1 -i $POST_IMG -i $POST_AUDIO -c:a aac -video_track_timescale 29971 -ac 1 -ab 112k -c:v libx264 -shortest -strict -2 $POST_VIDEO
-  
+  ffmpeg -y -loop 1 -i $POST_IMG -i $POST_AUDIO -c:a aac -video_track_timescale 29971 -ac 1 -ab 112k -c:v libx264 -shortest -strict -2 ${POST_VIDEO}.tmp.mp4
+  # reencode to a uniform format to ensure file sync
+  ffmpeg -i ${POST_VIDEO}.tmp.mp4 -acodec libvo_aacenc -vcodec libx264 -s $IMG_SIZE -r 60 -strict experimental $POST_VIDEO
+
   # append created file to our list
   echo Added video $POST_VIDEO to $VIDEO_LIST
   #echo ${POST_VIDEO} >> $VIDEO_LIST
@@ -92,10 +95,10 @@ done
 
 #ffmpeg -f concat -safe 0 -i $VIDEOS_LIST -c copy ${THREAD_WEBM}.mp4
 echo ******** generating video from video list $VIDEO_LIST *********
-ffmpeg -f concat -safe 0 -i $VIDEO_LIST -c copy test.mp4
+ffmpeg -y -f concat -safe 0 -i $VIDEO_LIST -c copy test.mp4
 
 
-ffmpeg -i test.mp4 ${THREAD_WEBM}
+ffmpeg -y -i test.mp4 ${THREAD_WEBM}
 
 if $UPLOAD ; then
   # upload to mixtape.moe
